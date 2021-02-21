f72198392d917e1e1310fc0e135244ba49de680a
NohaAshraf <noha_ashraf85@hotmail.com>
2021-02-20 21:16:25 -0500

    updated the CSS to make the text in the text area black, created an empty array so that so that when the button click event is clicked it will access the right text area in the array to save it, i still need to work on the local storage

--- Assets/scripts/scripts.js
+++ Assets/scripts/scripts.js
@@ -7,13 +7,25 @@ console.log(currentDay);
 var dt = new Date();
 var currentHour = dt.getHours();
 var currentRow = currentHour-9;
+var saveButton = $(".saveBtn");
 
 
 currentDay.text(moment().format("dddd, MMMM Do"));
 console.log(currentDay)
 
+var calendarTextArray =["", "", "", "", "", "", "", "", ""];
+
+
+
 $(".saveBtn").on("click", function() {
-    alert(this.value);
+    
+  
+    var text = $("#calendarTextArea" + this.value);
+    //This is the value in text area
+     calendarTextArray[this.value]=text.val();
+     console.log(calendarTextArray);
+    //"#calendarTextArea" + this.value
+    
   });
 
 for (var i=0; i < 9; i++){
