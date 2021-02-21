d00358fbdd291d5591e1b915746b0ba6db758dfb
NohaAshraf <noha_ashraf85@hotmail.com>
2021-02-21 11:19:20 -0500

    created the local storage, tested it and it is working

--- Assets/scripts/scripts.js
+++ Assets/scripts/scripts.js
@@ -16,17 +16,30 @@ console.log(currentDay)
 var calendarTextArray =["", "", "", "", "", "", "", "", ""];
 
 
-
-$(".saveBtn").on("click", function() {
-    
+$(".saveBtn").on("click", function(event) {
+    event.preventDefault();
   
     var text = $("#calendarTextArea" + this.value);
     //This is the value in text area
      calendarTextArray[this.value]=text.val();
      console.log(calendarTextArray);
     //"#calendarTextArea" + this.value
+
+    // set local storage
+    localStorage.setItem("calendarInput", JSON.stringify(calendarTextArray));
+
     
   });
+
+    var calendarFromLocalStorage = JSON.parse(localStorage.getItem("calendarInput"));
+    if (calendarFromLocalStorage !== null){
+        calendarTextArray = calendarFromLocalStorage;
+    }
+
+    for (var i = 0; i < calendarTextArray.length; i++)
+    {
+        $("#calendarTextArea" + i ) .val(calendarTextArray[i]); 
+    }
 
 for (var i=0; i < 9; i++){
     if (currentRow === i)
@@ -45,15 +58,6 @@ for (var i=0; i < 9; i++){
     }
 }
   
-
-
-
-
-
-
-
-
-
 
 
 });
